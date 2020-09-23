17b9b9e0da0101f2421d6c7bc59a6693d2f6fa41
swimmer-zone <yupsie@gmail.com>
2020-09-23 22:52:28 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,182 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Messages from './Layout/Messages';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	const birthday = '1986-03-01';
+
+	function calcAge(dateString) {
+	  	var birthday = +new Date(dateString);
+	  	return ~~((Date.now() - birthday) / (31557600000));
+	}
+
+	const years = [];
+	for (let i = (new Date().getFullYear() - 5); i >= (new Date().getFullYear() - 14); i--) {
+	    years.push(<option value={i} key={i}>{i}</option>);
+	}
+
+	const months = [];
+	for (let i = 1; i <= 12; i++) {
+	    months.push(<option value={i} key={i}>{i}</option>);
+	}
+
+	const days = [];
+	for (let i = 1; i <= 31; i++) {
+	    days.push(<option value={i} key={i}>{i}</option>)
+	}
+
+  	return (
+		<>
+		  	<header>
+				<img src="/logo.png" alt="logo" />
+				<h1>Weerbaar Worden</h1>
+				<p>Rots &amp; Watertraining voor jongens en meisjes</p>
+				<menu>
+					<li class="current"><a href="#welkom">Welkom</a></li>
+					<li><a href="#rots_en_water">Rots &amp; Water</a></li>
+					<li><a href="#training">Training</a></li>
+					<li><a href="#aanmelden">Meld je aan</a></li>
+				</menu>
+			</header>
+			<div>
+				<section id="welkom">
+					<p>
+						Welkom op de website van Weerbaar Worden.<br />Weerbaar Worden is een Rots- en Waterprogramma voor kinderen. Hier vind je alles wat je moet weten.
+					</p>
+				</section>
+				<section id="rots_en_water">
+					<h2>Wat is Rots &amp; Water?</h2>
+					<p>
+						Rots &amp; Water, een psychofysieke training voor jongens en meisjes voor basisonderwijs en voortgezet onderwijs, jeugdhulpverlening, jeugddetentie en GGZ.
+					</p>
+					<p>
+						Rots &amp; Water is een puur Nederlands programma, ontwikkeld door Freerk Ykema, dat momenteel wereldwijd wordt ingezet in de begeleiding van jongens en meisjes. Het Rots &amp; Water programma kan worden beschouwd als een weerbaarheidsprogramma, maar dan &eacute;&eacute;n dat zich onderscheidt van andere programma's door zijn meervoudige doelstelling en het bredere pedagogische perspectief waarbinnen de training van weerbaarheid samen gaat met de ontwikkeling van positieve sociale vaardigheden. Weerbaarheid en solidariteit, rots en water, worden in balans gepresenteerd en getraind. Anno 2010 zijn ruim 30.000 trainers, verspreid over de gehele wereld getraind.
+					</p>
+					<p>
+						Rots &amp; Water bestaat uit een basisprogramma, dat geschikt is voor leerlingen in het primair onderwijs (en ouder) en een vervolgprogramma dat op oudere leeftijdgroepen (14 jaar en ouder) is gericht. Rots en Water heeft als doel het verbeteren van zelfbeheersing, zelfreflectie, zelfvertrouwen, en communicatieve en sociale vaardigheden. Daarnaast zijn de preventie van geweld en van seksueel geweld, het leren maken van eigen keuzes en het leren gaan van een eigen weg belangrijke thema's. Rots en Water is oorspronkelijk ontwikkeld voor jongens maar wordt zowel in Nederland als ver daarbuiten nu ook ingezet voor meisjes. Rots en Water maakt gebruik van een psychofysieke didactiek, wat inhoudt dat startend vanuit een fysieke invalshoek, mentale en sociale vaardigheden worden aangereikt en verworven. Actie (spel, spelen en simpele zelfverdedigingsvormen) wordt afgewisseld door momenten van zelfreflectie en kringgesprekken.
+					</p>
+				</section>
+				<section id="training">
+					<h2>Indeling, inhoud en doelen</h2>
+
+					<ul>
+						<li><strong>7-8 jaar: </strong>Houding, stevig staan en technieken. Ogen-stemspellen. Zelfbewustzijn en keuzes maken.</li>
+						<li><strong>9-10 jaar: </strong>Houding, stevig staan en technieken. Veel spelvormen en oefenen. Sociaal bewustzijn, zelfbeheersing en zelfreflectie. Versterken eigenwaarde en in actie komen.</li>
+						<li><strong>11-12 jaar: </strong>Dezelfde indeling, inhoud en doelen als bij de 9 tot 10-jarigen, echter uitgebreid met het focussen op eigen doelen.</li>
+					</ul>
+
+					<h2>Wie ben ik?</h2>
+					<p>
+						Ik ben Marc van der Plaat en ben {calcAge(birthday)} jaar oud.
+					</p>
+
+					<h3>Gediplomeerd Rots &amp; Water trainer</h3>
+					<p>
+						Ik heb de PABO voor de vrijeschool gedaan en in 2012 heb ik mijn certificaat tot Rots &amp; Watertrainer behaald. Vanuit mijn liefde voor mens en ontwikkeling geef ik deze training.
+					</p>
+
+					<h2>Overige informatie</h2>
+					<ul>
+						<li>8 lessen van 1 uur</li>
+						<li>De groep zal bestaan uit 6 tot 8 deelnemers</li>
+						<li>Kosten: 150 euro</li>
+						<li>Ik ben preventief inzetbaar op scholen</li>
+					</ul>
+
+					<h3>Publicaties</h3>
+					<ul>
+						<li><a href="http://rotsenwater.nl/download/research/NL/onderzoek_klassen.pdf">Onderzoek naar effect van Rots en Water Programma [pdf]</a></li>
+						<li><a href="http://www.rotsenwater.nl/?action=main.content&article_nID=343&navID=2">Waarom scholen en maatschappelijke instellingen voor Rots en Water kiezen</a></li>
+					</ul>
+				</section>
+				<section id="aanmelden">
+					<h2>Meld je aan</h2>
+					<p>
+						Je kan je wensen vrijblijvend kenbaar maken via het onderstaande formulier. Je krijgt snel een reactie waarin je ge&iuml;nformeerd wordt over de laatste ontwikkelingen. Alle aanmeldingen worden verzameld en ingedeeld. Vervolgens stellen we (in overleg met de gegadigden) een agenda op. Er wordt een geschikte locatie bepaald en daarna volgt de mogelijkheid om de inschrijving definitief te maken.
+					</p>
+
+					<fieldset>
+						<legend>Vul het formulier in</legend>
+
+						<Messages type="error" message="Fout!" />
+
+						<form method="post" action="/post">
+							<input type="hidden" name="project" value="weerbaar" />
+							<input type="hidden" name="fill_this" />
+
+							<label for="naam_a">Naam</label>
+							<input type="text" name="naam[]" id="naam_a" />
+							<input type="text" name="naam[]" id="naam_b" /><br />
+
+							<label for="adres_a">Adres</label>
+							<input type="text" name="adres[]" id="adres_a" />
+							<input type="text" name="adres[]" id="adres_b" /><br />
+
+							<label for="postcode_a">Postcode</label>
+							<input type="text" name="postcode[]" id="postcode_a" />
+							<input type="text" name="postcode[]" id="postcode_b" /><br />
+
+							<label for="woonplaats">Woonplaats</label>
+							<input type="text" name="woonplaats" id="woonplaats" /><br />
+
+							<label for="leeftijd_a">Geboortedatum</label>
+							<select name="leeftijd[]" id="leeftijd_a">
+								<option>Dag</option>
+								{days}
+							</select>
+							<select name="leeftijd[]" id="leeftijd_b">
+								<option>Maand</option>
+								{months}
+							</select>
+							<select name="leeftijd[]" id="leeftijd_c">
+								<option>Jaar</option>
+								{years}
+							</select><br />
+
+							<label for="email">E-mail</label>
+							<input type="email" name="email" id="email" /><br />
+
+							<label for="telefoon_a">Telefoon</label>
+							+31 <input type="text" name="telefoon[]" id="telefoon_a" />
+							<input type="text" name="telefoon[]" id="telefoon_b" /><br />
+
+							<label for="leerdoel">Leerdoel</label>
+							<select name="leerdoel" id="leerdoel">
+								<option>Selecteer...</option>
+								<option value="Zeggen wat je denkt en voelt">Zeggen wat je denkt en voelt</option>
+								<option value="Sterk staan">Sterk staan</option>
+								<option value="Solidariteit">Solidariteit</option>
+								<option value="Assertiviteit">Assertiviteit</option>
+								<option value="Weerbaar voor de brugklas">Weerbaar voor de brugklas</option>
+								<option value="Meer zelfvertrouwen">Meer zelfvertrouwen</option>
+								<option value="Meer zelfbeheersing">Meer zelfbeheersing</option>
+								<option value="Meer zelfreflectie">Meer zelfreflectie</option>
+								<option value="Grenzen aangeven">Grenzen aangeven</option>
+								<option value="Grenzen aanvoelen">Grenzen aanvoelen</option>
+								<option value="Zelfverdediging">Zelfverdediging</option>
+							</select><br />
+
+							<label for="vragen">Vragen / Opmerkingen</label>
+							<textarea name="vragen" id="vragen"></textarea><br />
+							
+							<input type="submit" value="Verstuur" />
+						</form>
+					</fieldset>
+					<p className="footer">
+						<small>
+							Rots &amp; Water is een programma van het <a href="http://www.rotsenwater.nl">Rots &amp; Water Instituut NL</a>
+						</small>|
+						<small>
+							<a href="https://opendyslexic.org/">Open Dyslexic</a>
+						</small>|
+						<small>
+							&copy; <a href="http://swimmer.zone/">Swimmer</a> 2011 - {(new Date().getFullYear())}
+						</small>
+					</p>
+				</section>
+			</div>
+		</>
+  	);
 }
 
 export default App;
---
+++ src/Layout/Messages.js
@@ -0,0 +1,68 @@
+import React from 'react';
+
+const Messages = (type, message) => {
+    const types = {
+        error: (
+            <div class="toast toast_error">
+                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
+                    <path d="M16 2.899l13.409 26.726h-26.819l13.409-26.726zM16 0c-0.69 0-1.379 0.465-1.903 1.395l-13.659 27.222c-1.046 1.86-0.156 3.383 1.978 3.383h27.166c2.134 0 3.025-1.522 1.978-3.383h0l-13.659-27.222c-0.523-0.93-1.213-1.395-1.903-1.395v0z"></path>
+                    <path d="M18 26c0 1.105-0.895 2-2 2s-2-0.895-2-2c0-1.105 0.895-2 2-2s2 0.895 2 2z"></path>
+                    <path d="M16 22c-1.105 0-2-0.895-2-2v-6c0-1.105 0.895-2 2-2s2 0.895 2 2v6c0 1.105-0.895 2-2 2z"></path>
+                </svg>
+                <div class="message">{message}</div>
+            </div>
+        ),
+        warning: (
+            <div class="toast toast_warning">
+                <div class="message">{message}</div>
+            </div>
+        ),
+        note: (
+            <div class="toast toast_note">
+                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
+                    <path d="M14 9.5c0-0.825 0.675-1.5 1.5-1.5h1c0.825 0 1.5 0.675 1.5 1.5v1c0 0.825-0.675 1.5-1.5 1.5h-1c-0.825 0-1.5-0.675-1.5-1.5v-1z"></path>
+                    <path d="M20 24h-8v-2h2v-6h-2v-2h6v8h2z"></path>
+                    <path d="M16 0c-8.837 0-16 7.163-16 16s7.163 16 16 16 16-7.163 16-16-7.163-16-16-16zM16 29c-7.18 0-13-5.82-13-13s5.82-13 13-13 13 5.82 13 13-5.82 13-13 13z"></path>
+                </svg>
+                <div class="message">{message}</div>
+            </div>
+        ),
+        help: (
+            <div class="toast toast_help">
+                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
+                    <path d="M14 22h4v4h-4zM22 8c1.105 0 2 0.895 2 2v6l-6 4h-4v-2l6-4v-2h-10v-4h12zM16 3c-3.472 0-6.737 1.352-9.192 3.808s-3.808 5.72-3.808 9.192c0 3.472 1.352 6.737 3.808 9.192s5.72 3.808 9.192 3.808c3.472 0 6.737-1.352 9.192-3.808s3.808-5.72 3.808-9.192c0-3.472-1.352-6.737-3.808-9.192s-5.72-3.808-9.192-3.808zM16 0v0c8.837 0 16 7.163 16 16s-7.163 16-16 16c-8.837 0-16-7.163-16-16s7.163-16 16-16z"></path>
+                </svg>
+                <div class="message">{message}</div>
+            </div>
+        ),
+        chat: (
+            <div class="toast toast_chat">
+                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
+                    <path d="M16 2c8.837 0 16 5.82 16 13s-7.163 13-16 13c-0.849 0-1.682-0.054-2.495-0.158-3.437 3.437-7.539 4.053-11.505 4.144v-0.841c2.142-1.049 4-2.961 4-5.145 0-0.305-0.024-0.604-0.068-0.897-3.619-2.383-5.932-6.024-5.932-10.103 0-7.18 7.163-13 16-13z"></path>
+                </svg>
+                <div class="message">{message}</div>
+            </div>
+        ),
+        success: (
+            <div class="toast toast_success">
+                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
+                    <path d="M27 4l-15 15-7-7-5 5 12 12 20-20z"></path>
+                </svg>
+                <div class="message">{message}</div>
+            </div>
+        )
+    };
+
+    return (
+        <div class="toast toast_error">
+            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
+                <path d="M16 2.899l13.409 26.726h-26.819l13.409-26.726zM16 0c-0.69 0-1.379 0.465-1.903 1.395l-13.659 27.222c-1.046 1.86-0.156 3.383 1.978 3.383h27.166c2.134 0 3.025-1.522 1.978-3.383h0l-13.659-27.222c-0.523-0.93-1.213-1.395-1.903-1.395v0z"></path>
+                <path d="M18 26c0 1.105-0.895 2-2 2s-2-0.895-2-2c0-1.105 0.895-2 2-2s2 0.895 2 2z"></path>
+                <path d="M16 22c-1.105 0-2-0.895-2-2v-6c0-1.105 0.895-2 2-2s2 0.895 2 2v6c0 1.105-0.895 2-2 2z"></path>
+            </svg>
+            <div class="message">{types[type]}</div>
+        </div>
+    );
+}
+
+export default Messages;
